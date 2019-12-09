1f454e1fc944a689a5fdc22fd2c0a786cd7c848a
apple <abdullahsulam21@gmail.com>
2019-12-09 20:42:29 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_universe/ui/home_page.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "Planets",
+    home: new HomePage(),
+  ));
+}
